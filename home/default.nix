3981cb70814f578552cb4147fa930c39f09a44a2
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-11-10 18:15:23 +0800

    ～

--- home/default.nix
+++ home/default.nix
@@ -37,60 +37,60 @@
         -Dawt.toolkit.name=WLToolkit
       '';
     })
-    (pkgs.jetbrains.webstorm.override {
-      vmopts = ''
-        -Xms128m
-        -Xmx1024m
-        -XX:ReservedCodeCacheSize=512m
-        -XX:+IgnoreUnrecognizedVMOptions
-        -XX:+UseG1GC
-        -XX:SoftRefLRUPolicyMSPerMB=50
-        -XX:CICompilerCount=2
-        -XX:+HeapDumpOnOutOfMemoryError
-        -XX:-OmitStackTraceInFastThrow
-        -ea
-        -Dsun.io.useCanonCaches=false
-        -Djdk.http.auth.tunneling.disabledSchemes=""
-        -Djdk.attach.allowAttachSelf=true
-        -Djdk.module.illegalAccess.silent=true
-        -Dkotlinx.coroutines.debug=off
-        -XX:ErrorFile=$USER_HOME/java_error_in_idea_%p.log
-        -XX:HeapDumpPath=$USER_HOME/java_error_in_idea.hprof
+    # (pkgs.jetbrains.webstorm.override {
+    #   vmopts = ''
+    #     -Xms128m
+    #     -Xmx1024m
+    #     -XX:ReservedCodeCacheSize=512m
+    #     -XX:+IgnoreUnrecognizedVMOptions
+    #     -XX:+UseG1GC
+    #     -XX:SoftRefLRUPolicyMSPerMB=50
+    #     -XX:CICompilerCount=2
+    #     -XX:+HeapDumpOnOutOfMemoryError
+    #     -XX:-OmitStackTraceInFastThrow
+    #     -ea
+    #     -Dsun.io.useCanonCaches=false
+    #     -Djdk.http.auth.tunneling.disabledSchemes=""
+    #     -Djdk.attach.allowAttachSelf=true
+    #     -Djdk.module.illegalAccess.silent=true
+    #     -Dkotlinx.coroutines.debug=off
+    #     -XX:ErrorFile=$USER_HOME/java_error_in_idea_%p.log
+    #     -XX:HeapDumpPath=$USER_HOME/java_error_in_idea.hprof
 
-        --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED
-        --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED
+    #     --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED
+    #     --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED
 
-        -javaagent:/home/aaron/Documents/ja-netfilter/ja-netfilter.jar=jetbrains
-        -Dawt.toolkit.name=WLToolkit
-      '';
-    })
-    (pkgs.jetbrains.jetbrains.datagrip.override {
-      vmopts = ''
-        -Xms128m
-        -Xmx1024m
-        -XX:ReservedCodeCacheSize=512m
-        -XX:+IgnoreUnrecognizedVMOptions
-        -XX:+UseG1GC
-        -XX:SoftRefLRUPolicyMSPerMB=50
-        -XX:CICompilerCount=2
-        -XX:+HeapDumpOnOutOfMemoryError
-        -XX:-OmitStackTraceInFastThrow
-        -ea
-        -Dsun.io.useCanonCaches=false
-        -Djdk.http.auth.tunneling.disabledSchemes=""
-        -Djdk.attach.allowAttachSelf=true
-        -Djdk.module.illegalAccess.silent=true
-        -Dkotlinx.coroutines.debug=off
-        -XX:ErrorFile=$USER_HOME/java_error_in_idea_%p.log
-        -XX:HeapDumpPath=$USER_HOME/java_error_in_idea.hprof
+    #     -javaagent:/home/aaron/Documents/ja-netfilter/ja-netfilter.jar=jetbrains
+    #     -Dawt.toolkit.name=WLToolkit
+    #   '';
+    # })
+    # (pkgs.jetbrains.jetbrains.datagrip.override {
+    #   vmopts = ''
+    #     -Xms128m
+    #     -Xmx1024m
+    #     -XX:ReservedCodeCacheSize=512m
+    #     -XX:+IgnoreUnrecognizedVMOptions
+    #     -XX:+UseG1GC
+    #     -XX:SoftRefLRUPolicyMSPerMB=50
+    #     -XX:CICompilerCount=2
+    #     -XX:+HeapDumpOnOutOfMemoryError
+    #     -XX:-OmitStackTraceInFastThrow
+    #     -ea
+    #     -Dsun.io.useCanonCaches=false
+    #     -Djdk.http.auth.tunneling.disabledSchemes=""
+    #     -Djdk.attach.allowAttachSelf=true
+    #     -Djdk.module.illegalAccess.silent=true
+    #     -Dkotlinx.coroutines.debug=off
+    #     -XX:ErrorFile=$USER_HOME/java_error_in_idea_%p.log
+    #     -XX:HeapDumpPath=$USER_HOME/java_error_in_idea.hprof
 
-        --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED
-        --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED
+    #     --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED
+    #     --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED
 
-        -javaagent:/home/aaron/Documents/ja-netfilter/ja-netfilter.jar=jetbrains
-        -Dawt.toolkit.name=WLToolkit
-      '';
-    })
+    #     -javaagent:/home/aaron/Documents/ja-netfilter/ja-netfilter.jar=jetbrains
+    #     -Dawt.toolkit.name=WLToolkit
+    #   '';
+    # })
   ];
 
   home.stateVersion = "24.11";
