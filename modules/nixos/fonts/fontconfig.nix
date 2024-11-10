13bab81beeaf25dbd6f5402a2b738586fb1df0be
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-11-10 19:01:32 +0800

    ~

---
+++ modules/nixos/fonts/fontconfig.nix
@@ -0,0 +1,28 @@
+_:
+
+{
+  fonts = {
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        emoji = [
+          "Noto Color Emoji"
+        ];
+        serif = [
+          "GenSekiGothic TW R"
+          "Noto Serif CJK TC"
+          "Source Han Serif TC"
+        ];
+        sansSerif = [
+          "GenSekiGothic TW R"
+          "Noto Sans CJK TC"
+          "Source Han Sans TC"
+        ];
+        monospace = [
+          "JetBrainsMono Nerd Font"
+          "Noto Color Emoji"
+        ];
+      };
+    };
+  };
+}
