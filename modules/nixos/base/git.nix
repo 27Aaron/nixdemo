d2c43e49b2a1833bb91cc8d25325b79042ef3a3a
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-11-11 14:23:57 +0800

    =

--- modules/nixos/base/git.nix
+++ modules/nixos/base/git.nix
@@ -9,9 +9,6 @@
   # to make git use this config file, `~/.gitconfig` should not exist!
   #
   #    https://git-scm.com/docs/git-config#Documentation/git-config.txt---global
-  home.activation.removeExistingGitconfig = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
-    rm -f ${config.home.homeDirectory}/.gitconfig
-  '';
 
   programs.git = {
     enable = true;
