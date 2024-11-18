b81255943ff41364a3f7e36398986d07deca36fa
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-11-18 11:31:26 +0800

    =

--- hosts/beelink/firewall.nix
+++ hosts/beelink/firewall.nix
@@ -12,13 +12,9 @@
     ];
 
     extraCommands = ''
-      # 允许来自 192.168.122.0/24 子网的 SSH 访问
-      iptables -A INPUT -p tcp --dport 22 --source 192.168.122.0/24 -j ACCEPT
-      iptables -A INPUT -p tcp --dport 22 -j DROP
-
-      # 端口转发规则：将本地的 2222 端口流量转发到虚拟机的 22 端口
-      iptables -t nat -A PREROUTING -p tcp --dport 2222 -j DNAT --to-destination 192.168.122.102:22
-      iptables -A FORWARD -p tcp -d 192.168.122.102 --dport 22 -j ACCEPT
+      # 端口转发规则：将虚拟机的 22 端口流量转发到本地的 2222 端口
+      iptables -t nat -A PREROUTING -p tcp --dport 22 -d 192.168.122.102 -j DNAT --to-destination 127.0.0.1:2222
+      iptables -A FORWARD -p tcp -d 127.0.0.1 --dport 2222 -j ACCEPT
     '';
   };
 }
