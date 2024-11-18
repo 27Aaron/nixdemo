b250b4414f9a04852ac61361d8332b1639c08d5d
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-11-18 10:36:01 +0800

    +

--- hosts/beelink/firewall.nix
+++ hosts/beelink/firewall.nix
@@ -1,26 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  # 启用防火墙
-  networking.firewall.enable = true;
-
-  # 允许访问主机的 22 端口（SSH）
-  networking.firewall.allowedTCPPorts = [ 22 ];
-
-  # 配置端口转发
-  networking.firewall.nat = {
+  networking.firewall = {
+    # 启用防火墙
     enable = true;
-    extraRules = ''
-      # 将外部访问主机的 2222 端口流量转发到虚拟机的 22 端口
-      -A PREROUTING -p tcp --dport 2222 -j DNAT --to-destination 192.168.122.102:22
-      # 确保虚拟机的返回流量能够正确出站
-      -A POSTROUTING -s 192.168.122.102 -o enp2s0 -j MASQUERADE
+
+    # 添加端口转发规则
+    allowedTCPPorts = [
+      22
+      2222
+    ];
+
+    # 配置端口转发
+    extraCommands = ''
+      # 使用 iptables 配置端口转发
+      iptables -t nat -A PREROUTING -p tcp --dport 2222 -j DNAT --to-destination 192.168.122.102:22
+      iptables -A FORWARD -p tcp -d 192.168.122.102 --dport 22 -j ACCEPT
     '';
   };
-
-  # 允许访问虚拟机的 22 端口
-  networking.firewall.extraRules = ''
-    # 允许转发到虚拟机的 22 端口
-    -A FORWARD -p tcp -d 192.168.122.102 --dport 22 -j ACCEPT
-  '';
 }
